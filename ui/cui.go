cad1daadc2c467bf2e070b2b943b821aef993136
Fabio Falci <fabiofalci@gmail.com>
2014-08-01 08:57:36 +0100

    Show artist & track on track view

--- ui/cui.go
+++ ui/cui.go
@@ -225,7 +225,7 @@ func updateTracksView(g *gocui.Gui) {
 				playlistTrack := playlist.Track(i)
 				track := playlistTrack.Track()
 				track.Wait()
-				fmt.Fprintf(tracksView, "%v. %v", (i + 1), track.Name())
+				fmt.Fprintf(tracksView, "%v. %v - %v", (i + 1), track.Artist(0).Name(), track.Name())
 			}
 		}
 	}
