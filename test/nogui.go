72a297f6320e81bffb7bbb7a898312a03f17fba3
Fabio Falci <fabiofalci@gmail.com>
2014-09-08 12:29:24 +0100

    No gui go file for tests

---
+++ test/nogui.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/fabiofalci/sconsify/events"
+	"github.com/fabiofalci/sconsify/spotify"
+	"github.com/howeyc/gopass"
+)
+
+func main2() {
+	username, pass := credentials()
+	events := events.InitialiseEvents()
+
+	go spotify.Initialise(username, pass, events)
+	playlists := <-events.WaitForPlaylists()
+
+	playlist := playlists["Ramones"]
+	playlist.Wait()
+	track := playlist.Track(3).Track()
+	track.Wait()
+
+	events.ToPlay <- track
+
+	println(track.Name())
+
+	for {
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func credentials() (*string, *[]byte) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Username: ")
+	username, _ := reader.ReadString('\n')
+	username = strings.Trim(username, " \n\r")
+	fmt.Print("Password: ")
+	pass := gopass.GetPasswd()
+	return &username, &pass
+}
