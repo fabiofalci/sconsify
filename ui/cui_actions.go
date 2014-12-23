02d2a9801e4ebba8b31b9f005f318f7a5c16e034
Fabio Falci <fabiofalci@gmail.com>
2014-12-23 23:43:27 +0000

    Move keybindings method to actions

--- ui/cui_actions.go
+++ ui/cui_actions.go
@@ -7,6 +7,110 @@ import (
 	"github.com/jroimartin/gocui"
 )
 
+func keybindings() error {
+	views := []string{"main", "side", "queue"}
+	for _, view := range views {
+		if err := gui.g.SetKeybinding(view, 'p', 0, pauseCurrentSelectedTrack); err != nil {
+			return err
+		}
+		if err := gui.g.SetKeybinding(view, 'r', 0, setRandomMode); err != nil {
+			return err
+		}
+		if err := gui.g.SetKeybinding(view, 'R', 0, setAllRandomMode); err != nil {
+			return err
+		}
+		if err := gui.g.SetKeybinding(view, '>', 0, nextCommand); err != nil {
+			return err
+		}
+		if err := gui.g.SetKeybinding(view, '/', 0, enableSearchInputCommand); err != nil {
+			return err
+		}
+		if err := gui.g.SetKeybinding(view, 'j', 0, cursorDown); err != nil {
+			return err
+		}
+		if err := gui.g.SetKeybinding(view, 'k', 0, cursorUp); err != nil {
+			return err
+		}
+		if err := gui.g.SetKeybinding(view, 'q', 0, quit); err != nil {
+			return err
+		}
+	}
+
+	if err := gui.g.SetKeybinding("main", gocui.KeySpace, 0, playCurrentSelectedTrack); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("main", gocui.KeyEnter, 0, playCurrentSelectedTrack); err != nil {
+		return err
+	}
+
+	if err := gui.g.SetKeybinding("main", 'u', 0, queueCommand); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("queue", 'd', 0, removeFromQueueCommand); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("queue", 'D', 0, removeAllFromQueueCommand); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("status", gocui.KeyEnter, 0, searchCommand); err != nil {
+		return err
+	}
+
+	if err := gui.g.SetKeybinding("", gocui.KeyHome, 0, cursorHome); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("", gocui.KeyEnd, 0, cursorEnd); err != nil {
+		return err
+	}
+
+	if err := gui.g.SetKeybinding("", gocui.KeyPgup, 0, cursorPgup); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("", gocui.KeyPgdn, 0, cursorPgdn); err != nil {
+		return err
+	}
+
+	if err := gui.g.SetKeybinding("", gocui.KeyArrowDown, 0, cursorDown); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("", gocui.KeyArrowUp, 0, cursorUp); err != nil {
+		return err
+	}
+
+	if err := gui.g.SetKeybinding("main", gocui.KeyArrowLeft, 0, mainNextViewLeft); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("queue", gocui.KeyArrowLeft, 0, nextView); err != nil {
+		return err
+	}
+
+	if err := gui.g.SetKeybinding("side", gocui.KeyArrowRight, 0, nextView); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("main", gocui.KeyArrowRight, 0, mainNextViewRight); err != nil {
+		return err
+	}
+
+	if err := gui.g.SetKeybinding("main", 'h', 0, mainNextViewLeft); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("queue", 'h', 0, nextView); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("side", 'l', 0, nextView); err != nil {
+		return err
+	}
+	if err := gui.g.SetKeybinding("main", 'l', 0, mainNextViewRight); err != nil {
+		return err
+	}
+
+	if err := gui.g.SetKeybinding("", gocui.KeyCtrlC, 0, quit); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func playCurrentSelectedTrack(g *gocui.Gui, v *gocui.View) error {
 	track := getCurrentSelectedTrack()
 	if track != nil {
