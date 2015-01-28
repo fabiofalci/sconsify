212946cf5de18b047f9fc77baa1e739a2fddc108
Fabio Falci <fabiofalci@gmail.com>
2015-01-28 10:34:12 +0000

    Split playlists stuff from spotify

--- spotify/spotify.go
+++ spotify/spotify.go
@@ -3,7 +3,6 @@ package spotify
 import (
 	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	"code.google.com/p/portaudio-go/portaudio"
@@ -148,58 +147,5 @@ func (spotify *Spotify) waitForEvents() {
 		case query := <-spotify.events.SearchUpdates():
 			spotify.search(query)
 		}
-	}
-}
-
-func (spotify *Spotify) initPlaylist() error {
-	playlists := sconsify.InitPlaylists()
-
-	allPlaylists, err := spotify.session.Playlists()
-	if err != nil {
-		return err
-	}
-	allPlaylists.Wait()
-	for i := 0; i < allPlaylists.Playlists(); i++ {
-		playlist := allPlaylists.Playlist(i)
-		playlist.Wait()
-
-		if spotify.canAddPlaylist(playlist, allPlaylists.PlaylistType(i)) {
-			tracks := make([]*sconsify.Track, playlist.Tracks())
-			for i := 0; i < playlist.Tracks(); i++ {
-				playlistTrack := playlist.Track(i)
-				tracks[i] = sconsify.ToSconsifyTrack(playlistTrack.Track())
-			}
-			id := playlist.Link().String()
-			playlists.AddPlaylist(sconsify.InitPlaylist(id, playlist.Name(), tracks))
-		}
-	}
-
-	spotify.events.NewPlaylist(playlists)
-	return nil
-}
-
-func (spotify *Spotify) canAddPlaylist(playlist *sp.Playlist, playlistType sp.PlaylistType) bool {
-	return playlistType == sp.PlaylistTypePlaylist && spotify.isOnFilter(playlist.Name())
-}
-
-func (spotify *Spotify) isOnFilter(playlist string) bool {
-	if spotify.playlistFilter == nil {
-		return true
-	}
-	for _, filter := range spotify.playlistFilter {
-		if filter == playlist {
-			return true
-		}
-	}
-	return false
-}
-
-func (spotify *Spotify) setPlaylistFilter(playlistFilter string) {
-	if playlistFilter == "" {
-		return
-	}
-	spotify.playlistFilter = strings.Split(playlistFilter, ",")
-	for i := range spotify.playlistFilter {
-		spotify.playlistFilter[i] = strings.Trim(spotify.playlistFilter[i], " ")
 	}
 }
