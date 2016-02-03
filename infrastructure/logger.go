6a8771e12443b0bba52d6b751181c9204154c204
Fabio Falci <fabiofalci@gmail.com>
2016-02-03 22:21:50 +0000

    Logging just date time and file perm as 600

--- infrastructure/logger.go
+++ infrastructure/logger.go
@@ -11,9 +11,9 @@ var file *os.File
 func InitialiseLogger() {
 	filename := GetLogFileLocation()
 	if filename != "" {
-		file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 		if err == nil {
-			logger = log.New(file, "", log.Lshortfile)
+			logger = log.New(file, "", log.LstdFlags)
 		}
 	}
 }
